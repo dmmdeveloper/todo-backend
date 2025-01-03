d2280a7ee7d694d5efe54a0b59ba44f11fc4ed5e
dmmdeveloper <zongdostm@gmail.com>
2025-01-03 13:44:01 -0800

    .en-1v

--- src/middlewares/upload.middleware.js
+++ src/middlewares/upload.middleware.js
@@ -6,14 +6,14 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
 
-    destination : function (req , file , cb) {
+    destination : function (req , _ , cb) {
         cb(null , "/public/temp")        
     },
     filename : function (req , file , cb) {
         cb(null , file.originalname)   
     }
     
-})
+});
 
 
 
