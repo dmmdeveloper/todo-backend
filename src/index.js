10271acd071095e464e86b87219806a888e5c050
dmmdeveloper <zongdostm@gmail.com>
2025-01-03 08:14:11 -0800

    port

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import app from "./app.js"
 import dotenv from "dotenv";
 
 dotenv.config({path:".env"})
-const port = process.env.PORT || 2001
+const port = process.env.PORT;
 
 app.listen(port , ()=>{
     console.log(`=> app is listening http://localhost:${port}`);
