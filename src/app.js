2203e0c86c895f5d5b8df258f5f2d1c9b9c64b4f
dmmdeveloper <zongdostm@gmail.com>
2025-01-09 07:28:48 -0800

    profile logout

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ app.use(express.urlencoded({limit:"200kb" , extended:true}));
 app.use(express.static("public"));
 app.use(cookieParser());
 app.use(cors({
+
     // origin:"http://localhost:5173",
     origin:"https://todo-henna-psi.vercel.app",
 credentials:true,
